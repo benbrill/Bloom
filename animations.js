0d921e558a2695834db644ad6b0b13544ce3e08f
Ben Brill <benbrill101@gmail.com>
2020-11-22 19:57:32 -0800

    Update Java Script

--- animations.js
+++ animations.js
@@ -1,4 +1,8 @@
+
+
 $(document).ready(function() {
-  $(".scripttext").fadeIn(3000);
+  $(".scripttext").hide().first().fadeIn(3500);
+  $("#labox").hide().first().fadeIn(2000);
+  $("#explore").hide().delay(1000).fadeIn(2000)
 
 });
