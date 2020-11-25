928cd3234f2c746549a7d689edaf54bc7585e5a4
Ben Brill <benbrill101@gmail.com>
2020-11-24 20:52:34 -0800

    Add Parallax

--- animations.js
+++ animations.js
@@ -1,8 +1,15 @@
-
-
 $(document).ready(function() {
   $(".scripttext").hide().first().fadeIn(3500);
   $("#labox").hide().first().fadeIn(2000);
   $("#explore").hide().delay(1000).fadeIn(2000)
+
+  window.addEventListener('scroll', function() {
+    const target = document.querySelector('#labanner');
+
+    var scrolled = window.pageYOffset;
+    var rate = scrolled * 0.7;
+
+    target.style.transform = 'translate3d(0px,'+rate+'px, 0px)'
+  })
 
 });
